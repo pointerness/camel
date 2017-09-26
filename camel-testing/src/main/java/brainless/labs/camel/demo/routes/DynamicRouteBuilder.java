92081f55433273dd8d29fc97fb7f6e8396cb4565
pointerness <rohit.ven@gmail.com>
2017-09-26 21:51:02 +0530

    Added Interceptor testing dependencies for camel test profiles

--- camel-testing/src/main/java/brainless/labs/camel/demo/routes/DynamicRouteBuilder.java
+++ camel-testing/src/main/java/brainless/labs/camel/demo/routes/DynamicRouteBuilder.java
@@ -1,25 +1,40 @@
 package brainless.labs.camel.demo.routes;
 
+import java.io.InputStream;
+import java.util.stream.Collectors;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.GenericFile;
+import org.apache.camel.component.file.GenericFileConverter;
 
 import brainless.labs.camel.demo.config.Route;
 
-public class DynamicRouteBuilder extends RouteBuilder
-{
-    Route route;
+public class DynamicRouteBuilder extends RouteBuilder {
+	Route route;
 
-    public DynamicRouteBuilder(Route route)
-    {
-	super();
-	this.route = route;
-    }
+	public DynamicRouteBuilder(Route route) {
+		super();
+		this.route = route;
+	}
 
-    @Override
-    public void configure() throws Exception
-    {
-	String[] destinations = route.getDestinations().toArray(new String[route.getDestinations().size()]);
-	from(route.getSource().getPath()).routeId(route.getRouteId()).multicast().to(destinations);
+	@Override
+	public void configure() throws Exception {
+		interceptSendToEndpoint("azure-blob:*").process(new Processor() {
 
-    }
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				InputStream inputStream= GenericFileConverter.genericFileToInputStream((GenericFile<?>) exchange.getIn().getMandatoryBody(),
+						exchange);
+				exchange.getOut().setBody(inputStream);
+			}
+		});
+		// String[] destinations = route.getDestinations().toArray();
+		String[] destinations = new String[route.getDestinations().size()];
+		route.getDestinations().stream().map(r -> r.getPath()).collect(Collectors.toList()).toArray(destinations);
+		from(route.getSource().getPath()).routeId(route.getRouteId()).multicast().to(destinations);
+
+	}
 
 }
--- camel-testing/src/test/java/brainless/labs/camel/demo/test/RouteConfigurationTest.java
+++ camel-testing/src/test/java/brainless/labs/camel/demo/test/RouteConfigurationTest.java
@@ -5,17 +5,21 @@ import javax.annotation.Resource;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import brainless.labs.camel.demo.config.RouteConfig;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest()
+@ActiveProfiles("test")
 public class RouteConfigurationTest {
 	@Resource
 	RouteConfig routeConfig;
-
+	@Value("${camel.springboot.name}")
+	String camelContextName;
 	@Test
 	public void testConfig() {
 		Assert.assertNotNull(routeConfig);
